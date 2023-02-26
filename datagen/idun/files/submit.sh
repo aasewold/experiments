f2842f12fe4c8643a4dccb669765a76adc6bc30e
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2023-02-26 20:17:42 +0100

    datagen: reorganize submit script

--- datagen/idun/files/submit.sh
+++ datagen/idun/files/submit.sh
@@ -2,22 +2,25 @@
 
 set -euo pipefail
 
-routes_file=input/routes.csv
-num_routes=$(wc -l < $routes_file)
-
+# Setup output directory
 output_dir_name=$(date +%Y-%m-%d_%H-%M-%S)
 output_dir=output/$output_dir_name
 mkdir -p $output_dir
-
 (
     cd output
-    ln -sf $output_dir_name latest
+    ln -Tsf $output_dir_name latest
 )
 
-job_in=job.slurm
-job_out=$output_dir/job.slurm
-
-sed "s/#SBATCH --array=.*/#SBATCH --array=1-$num_routes/" < $job_in > $job_out
+# Copy all input data to output directory
+cp job.slurm datagen.sh $output_dir
+cp -r input $output_dir
 
 cd $output_dir
+
+# Calculate the correct number of nodes, one for each route file
+routes_file=input/routes.csv
+num_routes=$(wc -l < $routes_file)
+sed "s/#SBATCH --array=.*/#SBATCH --array=1-$num_routes/" -i job.slurm
+
+# Submit the job
 sbatch job.slurm
