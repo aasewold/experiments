2d3fa02ed084f04c1adf65ed9414f75749af8ece
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2023-03-18 23:26:32 +0100

    train-transfuser: resolve git branch names

--- train-transfuser/run.sh
+++ train-transfuser/run.sh
@@ -4,6 +4,9 @@ source ../common/utils.sh
 
 export DATASET_PATH="$1"; shift
 
+export CARLA_VERSION=0.9.14
+export TRANSFUSER_COMMIT="$(get_commit_hash experiments/${CARLA_VERSION})"
+
 if [ ! -d "$DATASET_PATH" ]; then
     echo "Dataset path does not exist: $DATASET_PATH"
     exit 1
