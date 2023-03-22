fd17ea64cff93cd93e0e15018c647e2901f50cce
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2023-03-22 10:27:29 +0100

    always use top-level 'models' dir

--- common/utils.sh
+++ common/utils.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-COMMON="$(dirname "$BASH_SOURCE")"
+COMMON="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+REPO="$(cd "$COMMON/.." && pwd)"
 
 run_in_screen() {
     screen -S "$1" -- \
@@ -26,28 +27,24 @@ setup_transfuser() (
     if ../scripts/check-prefuser.sh; then
         echo "Models already exist, skipping download"
     else
-        echo "Downloading models"
+        echo "Downloading pre-trained model"
         ../scripts/download-prefuser.sh
-    fi
-
-    if [ ! -h models/prefuser ]; then
-        echo "Creating symlink to models"
-        ln -s $(realpath ../models/prefuser) models/prefuser
     fi
 )
 
 run_transfuser() (
-    MODEL_PATH="models/$MODEL_NAME"
+    MODEL_PATH="$REPO/models/$MODEL_NAME"
     if [ ! -d "$MODEL_PATH" ]; then
         echo "Directory $MODEL_PATH does not exist"
         exit 1
     fi
 
     TRANSFUSER_COMMIT="$(get_commit_hash "$TRANSFUSER_COMMIT")"
+    MODEL_NAME_SUBST="$(echo "$MODEL_NAME" | tr , _)"
     CARLA_VERSION_SUBST="$(echo "$CARLA_VERSION" | tr . _)"
 
     screen_name="ex-${MODEL_NAME}-${CARLA_VERSION}"
-    compose_name="ex_${MODEL_NAME}_${CARLA_VERSION_SUBST}"
+    compose_name="ex_${MODEL_NAME_SUBST}_${CARLA_VERSION_SUBST}"
 
     RESULT_PATH="results/${MODEL_NAME}"
     mkdir -p "$RESULT_PATH"
