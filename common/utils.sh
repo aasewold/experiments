c0a5f1f587add12468d09008958669307871c201
Mathias Wold <45951843+mathiaswold@users.noreply.github.com>
2023-03-30 13:52:27 +0200

    interfuser: Refactor experiment scripts for 0.9.14

--- common/utils.sh
+++ common/utils.sh
@@ -96,3 +96,63 @@ run_transfuser() (
     run_in_screen "$screen_name" \
         "docker compose -p $compose_name -f $COMMON/transfuser.docker-compose.yml up --build"
 )
+
+run_interfuser() (
+    MODEL_PATH="$REPO/models/$MODEL_NAME"
+    if [ ! -d "$MODEL_PATH" ]; then
+        echo "Directory $MODEL_PATH does not exist"
+        exit 1
+    fi
+
+    if [ -z "$RESUME" ]; then
+        RUN_ID="$(date +%Y-%m-%dT%H-%M-%S)"
+        echo "Starting new run with ID \"$RUN_ID\""
+    else
+        RUN_ID="$RESUME"
+        echo "Resuming run with ID \"$RUN_ID\""
+    fi
+
+    sleep 1
+
+    RESULT_PATH="results/${MODEL_NAME}/${RUN_ID}"
+    echo Saving results to \"$RESULT_PATH\"
+
+    if [ ! -z "$RESUME" ] && [ ! -d "$RESULT_PATH" ]; then
+        echo "Directory $RESULT_PATH doesn't exists!"
+        exit 1
+    fi
+
+    mkdir -p "$RESULT_PATH"
+
+    INTERFUSER_COMMIT="$(get_commit_hash_interfuser "$INTERFUSER_COMMIT")"
+    MODEL_NAME_SUBST="$(echo "$MODEL_NAME" | tr , _)"
+    CARLA_VERSION_SUBST="$(echo "$CARLA_VERSION" | tr . _)"
+    RUN_ID_SUBST="$(echo "$RUN_ID" | tr '[:upper:]' '[:lower:]')"
+
+    screen_name="ex-${MODEL_NAME}-${CARLA_VERSION}-${EVALUATION}-${RUN_ID}"
+    compose_name="ex_${MODEL_NAME_SUBST}_${CARLA_VERSION_SUBST}-${EVALUATION}-${RUN_ID_SUBST}"
+
+    echo "# Time: $(date '+%Y-%m-%d %H:%M:%S')" >> "$RESULT_PATH/desc.txt"
+    echo "# Run ID: $RUN_ID" >> "$RESULT_PATH/desc.txt"
+    echo "# CARLA version: $CARLA_VERSION" >> "$RESULT_PATH/desc.txt"
+    echo "# Interfuser commit: $INTERFUSER_COMMIT" >> "$RESULT_PATH/desc.txt"
+    echo "# Model: $MODEL_NAME" >> "$RESULT_PATH/desc.txt"
+    echo "# Evaluation: $EVALUATION" >> "$RESULT_PATH/desc.txt"
+    echo >> "$RESULT_PATH/desc.txt"
+
+    echo "# Please write a short description of the run:" >> "$RESULT_PATH/desc.txt"
+    echo >> "$RESULT_PATH/desc.txt"
+    echo >> "$RESULT_PATH/desc.txt"
+
+    vim "$RESULT_PATH/desc.txt"
+
+    sleep 1
+
+    export CARLA_VERSION
+    export INTERFUSER_COMMIT
+    export MODEL_PATH="$(realpath "$MODEL_PATH")"
+    export RESULT_PATH="$(realpath "$RESULT_PATH")"
+
+    run_in_screen "$screen_name" \
+        "docker compose -p $compose_name -f $COMMON/interfuser.docker-compose.yml up --build"
+)
