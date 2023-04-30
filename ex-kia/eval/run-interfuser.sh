62b350c36c62d7236cbc87d7818aa02985888463
Mathias Wold <45951843+mathiaswold@users.noreply.github.com>
2023-04-30 21:02:58 +0200

    ex-kia: Update interfuser scripts

--- ex-kia/eval/run-interfuser.sh
+++ ex-kia/eval/run-interfuser.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+SCRIPT=$1; shift
+export TRIP=$1; shift
+
 source ../../common/utils.sh
 
 export CARLA_VERSION="0.9.14"
 export INTERFUSER_COMMIT=$(get_commit_hash_interfuser "0.9.14")
 export MODEL_PATH=../models/interfuser
-export SAVE_PATH="/work/viz-interfuser/$(date +%Y-%m-%d-%H-%M-%S)"
 
-mkdir -p "${SAVE_PATH:1}"
+export RUN="$(date +%Y-%m-%d-%H-%M-%S)"
+export RUN_PATH="work/out/$TRIP/$SCRIPT/$RUN"
+mkdir -p "$RUN_PATH/viz"
 
-docker compose -p eval-interfuser-kia -f interfuser.docker-compose.yml run -it --rm --build --user $UID interfuser python3 -m src.scripts."$@"
+docker compose -p eval-interfuser-kia -f interfuser.docker-compose.yml run -it --rm --build --user $UID interfuser python3 -m src.scripts.$SCRIPT $TRIP "$@"
