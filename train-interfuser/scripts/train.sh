c850ddc8396a3d8345c476ab1b12cb5a99a341f5
Mathias Wold <45951843+mathiaswold@users.noreply.github.com>
2023-05-01 16:01:48 +0200

    if train: Move dataset.py to scripts dir

--- train-interfuser/scripts/train.sh
+++ train-interfuser/scripts/train.sh
@@ -19,10 +19,8 @@ fi
 
 python3 -m torch.distributed.launch --nproc_per_node=$NUM_GPUS \
     train.py $DATASET_ROOT --dataset carla \
-    --train-towns 1 2 5 7 \
-    --val-towns 2 \
-    --train-weathers 0 2 \
-    --val-weathers 4 \
+    --train-towns 1 2 3 4 \
+    --train-weathers 0 2 4 6 7 11 \
     --model interfuser_baseline --sched cosine --epochs 25 --warmup-epochs 5 --lr 0.0005 --batch-size 16  -j 16 --no-prefetcher --eval-metric l1_error \
     --opt adamw --opt-eps 1e-8 --weight-decay 0.05  \
     --scale 0.9 1.1 --saver-decreasing --clip-grad 10 --freeze-num -1 \
