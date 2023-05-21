2305ebaf66fde94753704506d5b95fcd02654ca3
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2023-05-21 12:12:23 +0200

    transfuser eval: change carla image

--- ex-transfuser-0.9.14/run.sh
+++ ex-transfuser-0.9.14/run.sh
@@ -4,6 +4,8 @@ source ../common/utils.sh
 
 MODEL_NAME="$1"
 CARLA_VERSION=0.9.14
-TRANSFUSER_COMMIT=experiments/0.9.14
+TRANSFUSER_COMMIT=experiments/kia
+export SAVE_PATH=/results/viz
+export CARLA_IMAGE=mathiaswold/carla:0.9.14
 
 run_transfuser
