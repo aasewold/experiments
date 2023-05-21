3273ed0f5d7a9ca125bff5df8a1ec16ddd4dde5b
Mathias Wold <45951843+mathiaswold@users.noreply.github.com>
2023-05-21 14:54:06 +0200

    if train: New train and val town config

--- train-interfuser/scripts/train.sh
+++ train-interfuser/scripts/train.sh
@@ -19,8 +19,10 @@ fi
 
 python3 -m torch.distributed.launch --nproc_per_node=$NUM_GPUS \
     train.py $DATASET_ROOT --dataset carla \
-    --train-towns 1 2 3 4 \
-    --train-weathers 0 2 4 6 7 11 \
+    --train-towns 1 2 3 4 6 7 10 \
+    --val-towns 5 \
+    --train-weathers 0 2 4 6 7 11 12 14 17 20 \
+    --val-weathers 0 2 4 6 7 11 12 14 17 20 \
     --model interfuser_baseline --sched cosine --epochs 25 --warmup-epochs 5 --lr 0.0005 --batch-size 16  -j 16 --no-prefetcher --eval-metric l1_error \
     --opt adamw --opt-eps 1e-8 --weight-decay 0.05  \
     --scale 0.9 1.1 --saver-decreasing --clip-grad 10 --freeze-num -1 \
