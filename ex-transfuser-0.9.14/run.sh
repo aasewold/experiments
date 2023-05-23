c19c54068827953acec4a828bb146fd056a418f8
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2023-05-23 23:12:05 +0200

    ex-tf: move SAVE_PATH default into compose

--- ex-transfuser-0.9.14/run.sh
+++ ex-transfuser-0.9.14/run.sh
@@ -5,7 +5,6 @@ source ../common/utils.sh
 MODEL_NAME="$1"
 CARLA_VERSION=0.9.14
 TRANSFUSER_COMMIT=experiments/kia
-export SAVE_PATH=/results/viz
 export CARLA_IMAGE=mathiaswold/carla:0.9.14
 
 PS3='Select evaluation: '
